d4aa1d9c2f6ab29f271fbdd0573557a65d999ae8
TASH2019 <57631247+TASH2019@users.noreply.github.com>
2019-11-11 16:13:50 +0100

    Add files via upload

---
+++ TASH_DefineSubjects.m
@@ -0,0 +1,5 @@
+function [sub, Dload, Dsave] = TASH_DefineSubjects
+sub = {'fs_002GNV_V01'};
+Dload = '/home/josue2/data/freesurfer/subjects';
+Dsave = '/home/josue2/data/RestrictedFlatPatches/Nicolas';
+end
